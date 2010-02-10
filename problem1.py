232f329a7eddf42ff6ce595e21635bb1465474c6
Kushal Pisavadia <kushi.p@gmail.com>
2010-02-10 21:59:04 +0000

    refactored solution into a function, allowing variable limits

--- problem1.py
+++ problem1.py
@@ -1,7 +1,8 @@
-s = 0
-
-for i in range(1, 1000):
-	if i % 3 == 0 or i % 5 == 0:
-		s += i
-
-print s
+def natural_fizzbuzz(limit = 1000):
+	s = 0
+	
+	for i in range(1, limit):
+		if i % 3 == 0 or i % 5 == 0:
+			s += i
+	
+	print s
