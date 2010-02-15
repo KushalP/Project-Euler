0511898bc8e27a0d1968e9b23a7591938f1a39ca
Kushal Pisavadia <kushi.p@gmail.com>
2010-02-15 06:43:36 -0700

    method now returns the produced value rather than printing it and has a docblock for anyone that wants to use it in the future

--- Problems 1-9/problem1.py
+++ Problems 1-9/problem1.py
@@ -1,8 +1,7 @@
 def natural_fizzbuzz(limit = 1000):
+	"""Adds together all of the values divisible by 3 or 5 up to a given limit"""
 	s = 0
-	
 	for i in range(1, limit):
 		if i % 3 == 0 or i % 5 == 0:
 			s += i
-	
-	print s
+	return s
