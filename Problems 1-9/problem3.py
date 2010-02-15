6c16a2fefc4ca701a5cfc746f1806bd70bba5101
Kushal Pisavadia <kushi.p@gmail.com>
2010-02-15 23:51:18 +0000

    created a function which gets all of the prime factors of a given number and returns a set of these primes

--- Problems 1-9/problem3.py
+++ Problems 1-9/problem3.py
@@ -1,5 +1,12 @@
 def prime_factors(n = 600851475143):
 	"""Finds all of the prime factors of a given number"""
+	base = 2
+	while base < n:
+		if n % base == 0:
+			return [base] + prime_factors(n / base)
+		base += 1
+	return [n]
+	
 
 if __name__ == '__main__':
-	print "Answer: "
+	print "Answer: ", max(prime_factors(600851475143))
