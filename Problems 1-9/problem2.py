5bec509af1a692c21053cb69480279a0b1c55f57
Kushal Pisavadia <kushi.p@gmail.com>
2010-02-15 06:50:50 -0700

    function now returns values, rather than printing it and has now got a docblock

--- Problems 1-9/problem2.py
+++ Problems 1-9/problem2.py
@@ -1,8 +1,9 @@
 def fib(limit = 4000000):
+	"""Returns the cumulative sum of all the even Fibonacci numbers up to a given limit"""
 	a, b, s = 1, 0, 0
 	while s < limit:
 		a, b = a+b, a
 		if a % 2 == 0:
 			s += a
 	
-	print s
+	return s
