dea4af924af53eee9a19dadd2917a9015d06ca33
Kushal Pisavadia <kushi.p@gmail.com>
2010-03-24 22:30:27 +0000

    Laying out the palindrome problem. Can't use Python's nice splicing syntax so will have to use a clever loop with a reversing variable to store progress

---
+++ Problems 1-9/problem4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+static int is_palindrome(unsigned int n);
+
+int main()
+{
+	return 0;
+}
+
+int is_palindrome(unsigned int n)
+{
+	unsigned int reverse = 0, tester = n;
+
+	//Return a simple boolean
+	return reverse == n;
+}
