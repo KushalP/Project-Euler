35c52db09c81ed91286031a488ae46c499985053
Kushal Pisavadia <kushi.p@gmail.com>
2010-03-25 22:09:26 +0000

    again, copied the same implementation as used in the Python solution, but had to use a temporary variable as C doesn't have a nice variable switching method

--- Problems 1-9/problem5.c
+++ Problems 1-9/problem5.c
@@ -10,7 +10,15 @@ int main()
 
 unsigned long greatest_common_divisor(unsigned long x, unsigned long y)
 {
-	return y;
+	unsigned long temp_var;
+	while (y)
+	{
+		temp_var = x;
+		x = y;
+		y = temp_var % y;
+	}
+
+	return x;
 }
 
 unsigned long lowest_common_multiple(unsigned long x, unsigned long y)
