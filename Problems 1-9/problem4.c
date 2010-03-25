6a6f460aa0e924065ba6e8449f3061d4a2c0ff84
Kushal Pisavadia <kushi.p@gmail.com>
2010-03-25 21:28:44 +0000

    finished up reverse algorithm, which uses a simple decimal shift to find the reverse pattern of a given integer

--- Problems 1-9/problem4.c
+++ Problems 1-9/problem4.c
@@ -4,12 +4,23 @@ static int is_palindrome(unsigned int n);
 
 int main()
 {
+	unsigned int i, j, max = 0, upper_limit = 1000;
+
 	return 0;
 }
 
 int is_palindrome(unsigned int n)
 {
 	unsigned int reverse = 0, tester = n;
+
+	while (tester)
+	{
+		//shift digits left and grab the rightmost digit
+		reverse = 10 * reverse + (tester % 10);
+
+		//shift digits right
+		tester /= 10;
+	}
 
 	//Return a simple boolean
 	return reverse == n;
