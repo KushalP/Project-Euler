d72d7c58239f60625807e5c7a9c042b8cb77a756
Kushal Pisavadia <kushi.p@gmail.com>
2010-03-25 21:15:17 +0000

    finally implemented the palindrome main method, this takes an upper limit and will find the largest palindrome product of two numbers within the upper limit

--- Problems 1-9/problem4.py
+++ Problems 1-9/problem4.py
@@ -1,11 +1,23 @@
 def is_palindrome(n):
 	if isinstance(n, str):
 		# If we've been given a string, check if the reverse is the same
-		return n == n[::-1]
+		return (n == n[::-1])
 	else:
 		# We're going to convert our input to a string and reverse it
-		return str(n) == str(n)[::-1]
+		return (str(n) == str(n)[::-1])
+
+def find_largest_prime_number(upper_limit = 1000):
+	"""Find the largest palindrome made from the product of two 3-digit numbers"""
+	max_palindrome = 0
+
+	for x in xrange(0, upper_limit):
+		for y in xrange(0, upper_limit):
+			if (x * y) > max_palindrome and is_palindrome(x * y):
+				max_palindrome = x * y
+	
+	print "The largest palindrome product of numbers under %d is %d\n", (upper_limit, max_palindrome)
+	
+	return max_palindrome
 
 if __name__ == '__main__':
-	palindrome = "racecar"
-	print "Answer: is %s a palindrome?" % str(palindrome), is_palindrome(palindrome)
+	find_largest_prime_number()
