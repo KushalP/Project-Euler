626d1e9195b98d52bf693a11bbfd324a1d5630d5
Kushal Pisavadia <kushi.p@gmail.com>
2010-03-25 22:01:41 +0000

    finished implementing the smallest divisible function. had to increment any given upper limit by one as it wasn't enough loops to complete

--- Problems 1-9/problem5.py
+++ Problems 1-9/problem5.py
@@ -7,3 +7,18 @@ def greatest_common_divisor(x, y):
 def least_common_multiple(x, y):
 	"""Return the least common multiple of a given pair"""
 	return x * y / greatest_common_divisor(x, y)
+
+def smallest_evenly_divisible(upper_limit = 20):
+	"""What is the smallest number that is evenly divisble by all of the numbers from 1 to 20?"""
+	result = 1
+
+	for i in range(2, upper_limit + 1):
+		result = least_common_multiple(result, i)
+	
+	print "The smallest number evenly divisible by all numbers from 1 to %d is %d\n" %\
+		(upper_limit, result)
+
+	return result
+
+if __name__ == '__main__':
+        smallest_evenly_divisible()
