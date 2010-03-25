17080cd14e3b3f2498737b67ce18d7f98b90119a
Kushal Pisavadia <kushi.p@gmail.com>
2010-03-25 22:06:14 +0000

    layed out the base structure following my implementation of the same problem in Python

---
+++ Problems 1-9/problem5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+static unsigned long greatest_common_divisor(unsigned long x, unsigned long y);
+static unsigned long lowest_common_multiple(unsigned long x, unsigned long y);
+
+int main()
+{
+	return 0;
+}
+
+unsigned long greatest_common_divisor(unsigned long x, unsigned long y)
+{
+	return y;
+}
+
+unsigned long lowest_common_multiple(unsigned long x, unsigned long y)
+{
+}
