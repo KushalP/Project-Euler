e3dfa2376275e327e2b037f391975e16eea1ced3
Kushal Pisavadia <kushi.p@gmail.com>
2010-03-25 22:15:57 +0000

    created almost a like-for-like replica of the Python solution for the main method, the only difference being the printing and use of long unsigned int

--- Problems 1-9/problem5.c
+++ Problems 1-9/problem5.c
@@ -5,6 +5,15 @@ static unsigned long lowest_common_multiple(unsigned long x, unsigned long y);
 
 int main()
 {
+	unsigned long i, result = 1, upper_limit = 20;
+
+	for (i = 1; i < (upper_limit + 1); i++)
+	{
+		result = lowest_common_multiple(result, i);
+	}
+
+	printf("The smallest number evenly divisible by all numbers from 1 to %lu is %lu\n", upper_limit, result);
+
 	return 0;
 }
 
@@ -23,4 +32,5 @@ unsigned long greatest_common_divisor(unsigned long x, unsigned long y)
 
 unsigned long lowest_common_multiple(unsigned long x, unsigned long y)
 {
+	return (x * y) / greatest_common_divisor(x, y);
 }
