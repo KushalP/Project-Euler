90da89368b2ce5afdf072e7892bec14df5ac379f
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-04 00:04:21 +0100

    Used the sieve of eratosthenes to produce a set of prime numbers, used to calculate the nth prime number. It's not a perfect solution, but it will produce the expected result, eventually

--- Problems 1-9/problem7.py
+++ Problems 1-9/problem7.py
@@ -11,5 +11,16 @@ def sieve_of_eratosthenes(n = 10001):
 	#filter out the values that aren't set to zero
 	return [p for p in sieve if p]
 
+def find_nth_prime(n = 10001):
+	upper_limit = n + 1
+	primes = []
+
+	while len(primes) < n:
+		primes = sieve_of_eratosthenes(upper_limit)
+		upper_limit *= 2
+	
+	return primes[n-1]
+
+
 if __name__ == "__main__":
-	print sieve_of_eratosthenes()
+	print "The 10001st prime number is:", find_nth_prime()
