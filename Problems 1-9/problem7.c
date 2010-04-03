1980e26e359a821ffeb9d53bdafb03ff4dd6f3b9
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-04 00:20:39 +0100

    using the Python solution created using the Sieve of Eratosthenes as a basis for the C solution

---
+++ Problems 1-9/problem7.c
@@ -0,0 +1,20 @@
+#include <stdlib.h> 
+#include <stdio.h>
+
+unsigned int sieve_of_eratosthenes(unsigned int n);
+unsigned int find_nth_prime(unsigned int n);
+
+int main()
+{
+	return 0;
+}
+
+unsigned int sieve_of_eratosthenes(unsigned int n)
+{
+	return 1;
+}
+
+unsigned int find_nth_prime(unsigned int n)
+{
+	return 5;
+}
