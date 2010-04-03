a82663a38f5ff1c0be09c4acce1cdb21f6910587
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-03 22:30:33 +0100

    layed out the basic structure for the C implementation of problem 6

---
+++ Problems 1-9/problem6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+static unsigned long sum_of_squares(unsigned long n);
+static unsigned long square_of_sums(unsigned long n);
+
+int main()
+{
+}
+
+unsigned long sum_of_squares(unsigned long n)
+{
+	return 5;
+}
+
+unsigned long square_of_sums(unsigned long n)
+{
+	return 10;
+}
