cef1b91caf2d234c70941cb60d008ca890984727
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-03 22:25:29 +0100

    completed question 6 by creating two smaller functions that work out the sum of the squares and the square of the sums for a given number. This mean that I could abstract out as much of the calculation as possible

--- Problems 1-9/problem6.py
+++ Problems 1-9/problem6.py
@@ -1,5 +1,23 @@
-find_difference():
-	print "running main method"
+def sum_of_squares(n = 10):
+	sum = 0
+
+	for value in xrange(1, n+1):
+		sum += value**2
+	
+	return sum
+
+def square_of_sums(n = 10):
+	square = 0
+
+	for value in xrange(1, n+1):
+		square += value
+	
+	return square**2
+
+def find_difference():
+	print "The sum of squares is:", sum_of_squares(100)
+	print "The square of sums is:", square_of_sums(100)
+	print "The difference between the above is:", square_of_sums(100) - sum_of_squares(100)
 
 if __name__ == '__main__':
 	find_difference()
