4385b18a99fa8a4026ceac93dea1e74a6ddb045e
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-03 23:57:14 +0100

    implemented the sieve of eratosthenes using wikipedia and stackoverflow algorithm examples to produce a set of prime numbers

--- Problems 1-9/problem7.py
+++ Problems 1-9/problem7.py
@@ -1,5 +1,15 @@
 def sieve_of_eratosthenes(n = 10001):
-	return n
+	n = n + 1
+	sieve = range(n)
+	sieve[:2] = [0, 0]
+
+	for i in xrange(2, int(n**0.5) + 1):
+		if sieve[i]:
+			for j in xrange(i**2, n, i):
+				sieve[j] = 0
+
+	#filter out the values that aren't set to zero
+	return [p for p in sieve if p]
 
 if __name__ == "__main__":
 	print sieve_of_eratosthenes()
