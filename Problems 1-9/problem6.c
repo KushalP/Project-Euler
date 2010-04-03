fbd5a7d226a34a4502a29d757349f2a236ebf29a
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-03 23:39:56 +0100

    Completed implementation of problem 6 in C, largely following the methods layed out in the Python equivalent. The only difference being the heavy reliance on types, where an unsigned int (static) is used to make the method easier to use in the future

--- Problems 1-9/problem6.c
+++ Problems 1-9/problem6.c
@@ -5,14 +5,39 @@ static unsigned long square_of_sums(unsigned long n);
 
 int main()
 {
+	unsigned long n = 100;
+
+	printf("Your upper limit is: %lu\n", n);
+
+	printf("The sum of squares is: %lu\n", sum_of_squares(n));
+	printf("The square of sums is: %lu\n", square_of_sums(n));
+
+	printf("The difference between the above is: %lu\n", 
+		square_of_sums(n) - sum_of_squares(n));
+
+	return 0;
 }
 
 unsigned long sum_of_squares(unsigned long n)
 {
-	return 5;
+	unsigned long i, sum = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		sum += (i * i);
+	}
+
+	return sum;
 }
 
 unsigned long square_of_sums(unsigned long n)
 {
-	return 10;
+	unsigned long i, square = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		square += i;
+	}
+
+	return (square * square);
 }
