93593ec98a9ea6c38ade1973ba629da5d9f2c68d
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-03 22:27:14 +0100

    cleaned up the 'main' method by allowing an upper limit to be directly set across the board rather than extra code/line editing being needed

--- Problems 1-9/problem6.py
+++ Problems 1-9/problem6.py
@@ -14,10 +14,11 @@ def square_of_sums(n = 10):
 	
 	return square**2
 
-def find_difference():
-	print "The sum of squares is:", sum_of_squares(100)
-	print "The square of sums is:", square_of_sums(100)
-	print "The difference between the above is:", square_of_sums(100) - sum_of_squares(100)
+def find_difference(n = 100):
+	print "Your upper limit is:", n
+	print "The sum of squares is:", sum_of_squares(n)
+	print "The square of sums is:", square_of_sums(n)
+	print "The difference between the above is:", square_of_sums(n) - sum_of_squares(n)
 
 if __name__ == '__main__':
 	find_difference()
