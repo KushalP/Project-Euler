61f892616d1157fb39046fe583ed3ee5c69cdab2
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-04 11:37:19 +0100

    Created a list of pythagorean triplets for a given sum value. A product was then found by iterating through the list using a custom lambda reduce function. Where possible, any complex functionality that could need changing has been abstracted to it's own function

--- Problems 1-9/problem9.py
+++ Problems 1-9/problem9.py
@@ -1,8 +1,15 @@
 def is_pythagorean_triplet(a = 3, b = 4, product = 12):
 	return (a**2 + b**2 - (product - a - b)**2 == 0)
 
-def find_pythagorean_triplet_product(n = 1000):
-	return is_pythagorean_triplet()
+def find_pythagorean_triplet(n = 1000):
+	for a in xrange(1, n):
+		for b in xrange(1, n):
+			if is_pythagorean_triplet(a, b, n):
+				c = n - a - b
+				return [a, b, c]
 
 if __name__ == "__main__":
-	print find_pythagorean_triplet_product()
+	triplet_product = 1000
+	triplet_list = find_pythagorean_triplet(triplet_product)
+	product = reduce(lambda y,x: x*y, triplet_list)
+	print "The product of the Pythagorean triplet with sum %d is %d" % (triplet_product, product)
