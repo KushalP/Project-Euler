13d89876508cb20b7a8f4e1fbfe66972f6750249
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-04 01:01:26 +0100

    Created a solution which is slightly less useful in comparison to it's Python equivalent, but managed to contain boolean sets in the form of a char* [character array]. The only issue I found involved cleaning up memory once the function has finished, which is where there are duplicated free() calls. These need to be dealt with to prevent reusability issues down the line.

--- Problems 1-9/problem7.c
+++ Problems 1-9/problem7.c
@@ -1,20 +1,47 @@
-#include <stdlib.h> 
 #include <stdio.h>
+#include <stdlib.h> 
 
 unsigned int sieve_of_eratosthenes(unsigned int n);
-unsigned int find_nth_prime(unsigned int n);
 
 int main()
 {
+	int n = 10001;
+
+	printf("The 10001st prime number is: %u\n", sieve_of_eratosthenes(n));
+
 	return 0;
 }
 
 unsigned int sieve_of_eratosthenes(unsigned int n)
 {
-	return 1;
-}
+	char *sieve;
+	unsigned int i, upper_limit = 1000000;
+	unsigned count = 0;
+	
+	sieve = calloc(n, sizeof *sieve); //Initialises to zero
 
-unsigned int find_nth_prime(unsigned int n)
-{
-	return 5;
+	for (i = 2; i < upper_limit; i++)
+	{
+		if (!sieve[i])
+		{
+			unsigned int j;
+
+			count++;
+
+			if (count == n)
+			{
+				free(sieve); //we've reached our nth prime, clean up memory
+				return i;
+			}
+
+			for (j = i*2; j < upper_limit; j += i)
+			{
+				sieve[j] = 1;
+			}
+		}
+	}
+
+	free(sieve);
+
+	return 0;
 }
