1070b0a530fb2fa86145858f40d141a85276affd
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-04 13:50:51 +0100

    had to embed the method within the solution file as Python relative imports seem to be a grey area until Python3 comes out and there's no point wasting too much time on it at the moment

--- Problems 10-19/problem10.py
+++ Problems 10-19/problem10.py
@@ -1,4 +1,15 @@
-from problem7 import sieve_of_eratosthenes
+def sieve_of_eratosthenes(n = 10001):
+	n = n + 1
+	sieve = range(n)
+	sieve[:2] = [0, 0]
+
+	for i in xrange(2, int(n**0.5) + 1):
+		if sieve[i]:
+			for j in xrange(i**2, n, i):
+				sieve[j] = 0
+
+	#filter out the values that aren't set to zero
+	return [p for p in sieve if p]
 
 def sum_of_primes_below(n = 2000000):
 	sum_of_primes = sum(sieve_of_eratosthenes(n))
