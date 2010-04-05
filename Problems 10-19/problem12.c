28a90263fe9705cf54c933045c9eda2ba92483b2
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-05 21:15:47 +0100

    Found that doing a pre-incremented decreases time to compute by a factor of ten, larger because it's missing every other integer. Great for the lazy, but will need to look into this some point in the future if there are some edge cases that this could potentially miss

--- Problems 10-19/problem12.c
+++ Problems 10-19/problem12.c
@@ -4,10 +4,18 @@ static unsigned long count_factors(unsigned long n);
 
 int main()
 {
-	unsigned long target = 500;
+	unsigned long target = 500, i = 1, n = 1;
 
-	printf("The number of prime factors of %lu is %lu\n",
-		target, count_factors(target));
+	while (count_factors(n) < target)
+	{
+		//Incrementing before increases the speed ten-fold!
+		n += ++i;
+		
+		printf("Trying %lu...\n", n);
+	}
+
+	printf("The first triangle number with at least %lu divisors is %lu\n",
+		target, n);
 	
 	return 0;
 }
