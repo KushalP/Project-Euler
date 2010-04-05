9b4f1f6da33122d4803f454323e83a63ff61c38f
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-05 18:33:03 +0100

    Created a function which will count the total number of prime factors for a given integer, this should speed up the loop, allowing a simple 'greater than' statement to exit at our desired value

--- Problems 10-19/problem12.py
+++ Problems 10-19/problem12.py
@@ -1,6 +1,11 @@
 def find_factors(n = 100):
-	return [i for i in xrange(1, n**0.5 + 1) if not n % i] + [n]
+	"""Produce a set of all the prime factors of a given integer"""
+	return [i for i in xrange(1, n//2 + 1) if not n % i] + [n]
+
+def count_factors(n = 100):
+	"""Count the number of prime factors of a given integer"""
+	return len(find_factors(n))
 
 if __name__ == "__main__":
-	for i in xrange(1, 50):
-		print "The number of factors for %d is:" % (i), find_factors(i)
+	target = 500
+	print "The integer %d has %d prime factors" % (target, count_factors(target))
