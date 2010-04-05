69b63c22fa50ae4e42d069c4a4e7e8fa4e04419f
Kushal Pisavadia <kushi.p@gmail.com>
2010-04-05 19:12:18 +0100

    Brute forced the solution to problem 12 by using the definition of what a 'Triangle Number' is off of the internet and iterating through each case which fits this

--- Problems 10-19/problem12.py
+++ Problems 10-19/problem12.py
@@ -6,6 +6,28 @@ def count_factors(n = 100):
 	"""Count the number of prime factors of a given integer"""
 	return len(find_factors(n))
 
+def find_triangle_number_with_divisors(target = 500):
+	n = target
+	last_factors = count_factors(n - 1)
+
+	while 1:
+		factors = count_factors(n)
+
+		if factors * last_factors - max(factors, last_factors) >= target:
+			print "Trying %d * %d / 2" % (n - 1, n)
+			triangle_factors = count_factors(n * (n - 1) / 2)
+
+			if triangle_factors <= target:
+				print "only %d factors, continuing..." % triangle_factors
+				n += 1
+				continue
+
+			#We know that all Triangle Numbers fit the equation n * (n - 1) / 2
+			return n * (n - 1) / 2
+
+		last_factors = factors
+		n += 1
+
 if __name__ == "__main__":
 	target = 500
-	print "The integer %d has %d prime factors" % (target, count_factors(target))
+	print "The first triangle number with at least %d divisors is %d" % (target, find_triangle_number_with_divisors(target))
