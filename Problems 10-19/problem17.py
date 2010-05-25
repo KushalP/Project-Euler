85d9dbf8e7f04514c393c7b3866ced542cdab376
Kushal Pisavadia <kushi.p@gmail.com>
2010-05-25 23:18:39 +0100

    Completed the Python solution to 17 largly using brute force

--- Problems 10-19/problem17.py
+++ Problems 10-19/problem17.py
@@ -37,5 +37,34 @@ def count_letters_in_words(upper_bound = 1000):
 		0: 0
 	}
 
+	hundreds_sum = len("hundred")
+	letter_sum = 0;
+
+	for i in xrange(1, 10):
+		letter_sum += digits[i]
+	
+	for i in xrange(10, 100):
+		if unique_numbers.has_key(i):
+			letter_sum += unique_numbers[i]
+		else:
+			letter_sum += tens_modifiers[i - i % 10] + digits[i % 10]
+
+	for i in xrange(100, 1000):
+		letter_orig = letter_sum
+		letter_sum += digits[i / 100]
+		tens = tens_modifiers[i % 100 - i % 10] + digits[i % 10]
+
+		if unique_numbers.has_key(i % 100):
+			tens = unique_numbers[i % 100]
+
+		if tens != 0:
+			letter_sum += tens + hundreds_sum + 3
+		else:
+			letter_sum += hundreds_sum
+	
+	letter_sum += unique_numbers[1000]
+	
+	return letter_sum
+
 if __name__ == "__main__":
-	print "No method yet"
+	print "If you wrote the numbers from 1 to 1000 and counted the letters, it would equal:", count_letters_in_words()
